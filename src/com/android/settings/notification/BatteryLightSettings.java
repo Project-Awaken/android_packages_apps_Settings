555ec8688cecc273d6ca6f20e6adac8144bc102f
ezio84 <brabus84@gmail.com>
2017-09-11 06:46:31 +0530

    Settings: Battery light customization [2/2]
    Change-Id: I792c6428a774fb92b981e4e248d024c45acd6f4e
    
    -----
    
    Settings: Move battery light settings into notification section
    
     * also implement a check to remove the preference completly on no LED config
    
    Change-Id: I11775f8728c0de5f3e3f74f47cb84bc38d985e98
    
    -----
    
    Settings: Battery light color: Show reset to default button
    
    Change-Id: I8c3e37ba859e29ec7f4c2cbed563aa6bd3fb14c6
    
    -----
    
    Settings: Battery LED: Activate led light preview
    
    Change-Id: I32c47955b09e43b35e8f9c4417f30f6235a45561
    
    -----
    
    Settings: Disable useless alphaslider for battery led customization
    
    Change-Id: I9ac6f1c9987ecc55a2e18f7f9892c91eb8d3dc1f
    
    -----
    
    Settings: Enable notifications led light by default [2/2]
    
    Change-Id: I10e731a0739e42b69a7ac0f90c78bd062c878f68
    
    -----
    
    Settings: Add option to disable led after full charging [2/2]
    
    Change-Id: I065c9936d27ebfadb978db03901812e0a399aaef
    Co-authored-by: ganeshi4u <ganeshvarma@arrowos.net>
    Co-authored-by: xyyx <xyyx@mail.ru>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

---
+++ src/com/android/settings/notification/BatteryLightSettings.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2018 ArrowOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.awaken.support.preferences.SystemSettingSwitchPreference;
+
+public class BatteryLightSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private SystemSettingSwitchPreference mLowBatteryBlinking;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.configure_notification_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLowBatteryBlinking = (SystemSettingSwitchPreference)prefSet.findPreference("battery_light_low_blinking");
+        if (getResources().getBoolean(
+                        com.android.internal.R.bool.config_ledCanPulse)) {
+            mLowBatteryBlinking.setChecked(Settings.System.getIntForUser(getContentResolver(),
+                            Settings.System.BATTERY_LIGHT_LOW_BLINKING, 0, UserHandle.USER_CURRENT) == 1);
+            mLowBatteryBlinking.setOnPreferenceChangeListener(this);
+        } else {
+            prefSet.removePreference(mLowBatteryBlinking);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.AWAKEN;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLowBatteryBlinking) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_LOW_BLINKING, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mLowBatteryBlinking.setChecked(value);
+        }
+        return false;
+    }
+}
