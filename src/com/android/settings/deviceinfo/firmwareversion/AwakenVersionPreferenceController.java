71b20eb432cf4279e4dc2e1b1d95accfbeff75b5
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-09-12 16:20:35 +0200

    Settings: Show build release type alongside ROM version
    * Based on https://github.com/ArrowOS/android_packages_apps_Settings/commit/a466a7ef434a79bcc0d7403bc46455c8558c5692
    
    Change-Id: I5299d53167137d649060104d1942858e0c6b3c43
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/AwakenVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/AwakenVersionPreferenceController.java
@@ -43,6 +43,7 @@ public class AwakenVersionPreferenceController extends BasePreferenceController 
     private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
     private static final String KEY_AWAKEN_VERSION_PROP = "ro.awaken.base.version";
+    private static final String ROM_RELEASETYPE_PROP = "ro.awaken.build.type";
 
     private final UserManager mUserManager;
     private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
@@ -73,8 +74,14 @@ public class AwakenVersionPreferenceController extends BasePreferenceController 
 
     @Override
     public CharSequence getSummary() {
-        return SystemProperties.get(KEY_AWAKEN_VERSION_PROP,
+        String awakenVersion = SystemProperties.get(KEY_AWAKEN_VERSION_PROP,
                 mContext.getString(R.string.unknown));
+        String awakenReleasetype =  SystemProperties.get(ROM_RELEASETYPE_PROP,
+                this.mContext.getString(R.string.unknown));
+        if (!awakenVersion.isEmpty() && !awakenReleasetype.isEmpty())
+            return awakenVersion + " | " + awakenReleasetype;
+        else
+            return mContext.getString(R.string.awaken_version_default);
     }
 
     @Override
