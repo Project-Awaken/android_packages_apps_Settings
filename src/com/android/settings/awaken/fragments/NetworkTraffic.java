0274eb5934c0ba09408092e9d8ace6b803707eed
Anushek Prasal <anushekprasal@gmail.com>
2020-05-18 00:49:11 +0530

    Settings: Network Traffic: Allow to choose display mode [1/2]
    Change-Id: I7972c102b0e41d181426bf1635ed7ba9ee8e52ba
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- src/com/android/settings/awaken/fragments/NetworkTraffic.java
+++ src/com/android/settings/awaken/fragments/NetworkTraffic.java
@@ -38,6 +38,7 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private SystemSettingListPreference mLocation;
+    private SystemSettingListPreference mIndicatorMode;
     private SystemSettingSeekBarPreference mThreshold;
     private SystemSettingSeekBarPreference mInterval;
 
@@ -47,6 +48,7 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.network_traffic);
 
         mLocation = (SystemSettingListPreference) findPreference("network_traffic_location");
+        mIndicatorMode = (SystemSettingListPreference) findPreference("network_traffic_mode");
         mThreshold = (SystemSettingSeekBarPreference) findPreference("network_traffic_autohide_threshold");
         mInterval = (SystemSettingSeekBarPreference) findPreference("network_traffic_refresh_interval");
     }
