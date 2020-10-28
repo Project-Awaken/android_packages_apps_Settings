a94ad40261b1c4054cc8d55f34b0f4e67ef24f4c
SKULSHADY <anushekprasal@gmail.com>
2020-10-28 13:39:01 +0530

    Settings: Add OnePlus dashboard icons [1/2]
    @saikiran2001 - Adapt to android 10 and android 11
    
    Co-authored-by: Saikiran <bjsaikiran@gmail.com>
    Change-Id: I6583f2b71c867fa87965fb86010ed44137ce0abc

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -171,6 +171,10 @@ public class TopLevelSettings extends DashboardFragment implements
                         aIcon.setForegroundColorAicp(mAccentColor);
                         aIcon.setBackgroundColorAicp(0);
                         break;
+                    case 4:
+                        aIcon.setForegroundColorAicp(mAccentColor);
+                        aIcon.setBackgroundColorAicp(R.color.settings_icon_oneplus);
+                        break;
                 }
             } else if (icon instanceof LayerDrawable) {
                 LayerDrawable lIcon = (LayerDrawable) icon;
@@ -191,6 +195,10 @@ public class TopLevelSettings extends DashboardFragment implements
                         case 3:
                             fg.setTint(mAccentColor);
                             bg.setTint(0);
+                            break;
+                        case 4:
+                            fg.setTint(mAccentColor);
+                            bg.setTint(R.color.settings_icon_oneplus);
                             break;
                     }
                 }
