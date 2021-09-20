c6dcfe25eeceb5606f4b7831203766173ea0ca99
Anushek Prasal <anushekprasal@gmail.com>
2021-09-21 00:01:17 +0530

    Settings: Battery stats: Display battery health
    Change-Id: I7f149d2b6a68baef53b717066c951a7d865ec436
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -28,6 +28,7 @@ import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings.Global;
+import android.text.TextUtils;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
@@ -54,6 +55,9 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.LayoutPreference;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -73,6 +77,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
 
     private static final String KEY_BATTERY_CHARGING_LIGHT = "battery_charging_light";
     private static final String KEY_BATTERY_TEMP = "battery_temp";
+    private static final String KEY_BATTERY_HEALTH = "battery_health";
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -92,6 +97,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
     Preference mBatteryLightPref;
     @VisibleForTesting
     PowerGaugePreference mBatteryTempPref;
+    @VisibleForTesting
+    PowerGaugePreference mBatteryHealthPref;
 
     @VisibleForTesting
     BatteryHeaderPreferenceController mBatteryHeaderPreferenceController;
@@ -103,6 +110,10 @@ public class PowerUsageSummary extends PowerUsageBase implements
     Preference mHelpPreference;
     @VisibleForTesting
     Preference mBatteryUsagePreference;
+
+    private String mBatteryHealth;
+    private String mBatteryRemainingCapacity;
+    private String mBatteryDesignCapacity;
 
     @VisibleForTesting
     final ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
@@ -181,7 +192,14 @@ public class PowerUsageSummary extends PowerUsageBase implements
         initFeatureProvider();
         initPreference();
 
+        mBatteryHealthPref = (PowerGaugePreference) findPreference(KEY_BATTERY_HEALTH);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
+
+        mBatteryHealth = getResources().getString(R.string.config_batteryHealthNode);
+        mBatteryRemainingCapacity = getResources().getString(R.string.config_batteryRemainingCapacityNode);
+        mBatteryDesignCapacity = getResources().getString(R.string.config_batteryDesignCapacityNode);
+
+        mBatteryHealthPref.setVisible(getBatteryHealth() != null);
 
         if (Utils.isBatteryPresent(getContext())) {
             restartBatteryInfoLoader();
@@ -312,6 +330,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
         mBatteryTempPref.setSummary(BatteryInfo.batteryTemp + " \u2103");
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
+        if (mBatteryHealthPref != null)
+            mBatteryHealthPref.setSummary(getBatteryHealth() + "%");
     }
 
     @VisibleForTesting
@@ -322,6 +342,20 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @VisibleForTesting
     void setBatteryLayoutPreference(LayoutPreference layoutPreference) {
         mBatteryLayoutPref = layoutPreference;
+    }
+
+    String getBatteryHealth() {
+        String health;
+        if (!TextUtils.isEmpty(mBatteryHealth)) {
+            health = readLine(mBatteryHealth);
+        } else if (!TextUtils.isEmpty(mBatteryRemainingCapacity) &&
+                !TextUtils.isEmpty(mBatteryDesignCapacity)) {
+            health = String.valueOf(Integer.parseInt(readLine(mBatteryRemainingCapacity)) * 100 /
+                    Integer.parseInt(readLine(mBatteryDesignCapacity)));
+        } else {
+            health = null;
+        }
+        return health;
     }
 
     @VisibleForTesting
@@ -380,6 +414,22 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @Override
     public void onBatteryTipHandled(BatteryTip batteryTip) {
         restartBatteryTipLoader();
+    }
+
+    private static String readLine(String filename) {
+        BufferedReader reader;
+        String line = null;
+        try {
+            reader = new BufferedReader(new FileReader(filename), 256);
+            try {
+                line = reader.readLine();
+            } finally {
+                reader.close();
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return line;
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
