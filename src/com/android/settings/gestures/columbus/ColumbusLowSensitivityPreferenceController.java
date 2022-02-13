81967bf6dae544f7b88e6b62505a76068cabd335
jhenrique09 <jhsv09@gmail.com>
2022-02-13 01:23:47 -0300

    ColumbusLowSensitivityPreferenceController: Disable if using ap sensor
    Change-Id: I0f0fe01c1e92860270c956107a1fca64f6e0de08

---
+++ src/com/android/settings/gestures/columbus/ColumbusLowSensitivityPreferenceController.java
@@ -0,0 +1,28 @@
+package com.android.settings.gestures.columbus;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import com.android.settings.R;
+import com.android.settings.slices.SliceBackgroundWorker;
+import com.google.android.settings.gestures.columbus.ColumbusTogglePreferenceController;
+
+import android.os.SystemProperties;
+
+public class ColumbusLowSensitivityPreferenceController extends ColumbusTogglePreferenceController {
+    public ColumbusLowSensitivityPreferenceController(Context context, String str) {
+        super(context, str, 1747);
+    }
+
+    @Override // com.android.settings.core.BasePreferenceController
+    public int getAvailabilityStatus() {
+        if (SystemProperties.getBoolean("persist.columbus.use_ap_sensor", true)){
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        return super.getAvailabilityStatus();
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_system;
+    }
+}
