198a73f6edbdebc8db8ba00cd6797ebee8789c7d
PainKiller3 <ninadpatil100@gmail.com>
2022-06-13 06:16:14 +0200

    Settings: Nuke GoogleGameDashboard in favour of GameSpace
    This reverts commit 7479c581d756ec50dcfd413ea21d91d9a4c40eb4 & 68f2a4e7623a403278e5bf9be2bdedb94e883206.
    
    Change-Id: Ia5867f77367fe58caf702dbc3486363c4c997008

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -3,14 +3,11 @@ package com.google.android.settings.overlay;
 import android.content.Context;
 
 import com.android.settings.accounts.AccountFeatureProvider;
-import com.android.settings.applications.GameSettingsFeatureProvider;
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
-import com.google.android.settings.games.GameSettingsFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
 
     private AccountFeatureProvider mAccountFeatureProvider;
-    private GameSettingsFeatureProvider mGameSettingsFeatureProvider;
 
     @Override
     public AccountFeatureProvider getAccountFeatureProvider() {
@@ -18,13 +15,5 @@ public final class FeatureFactoryImpl extends com.android.settings.overlay.Featu
             mAccountFeatureProvider = new AccountFeatureProviderGoogleImpl();
         }
         return mAccountFeatureProvider;
-    }
-
-    @Override
-    public GameSettingsFeatureProvider getGameSettingsFeatureProvider() {
-        if (mGameSettingsFeatureProvider == null) {
-            mGameSettingsFeatureProvider = new GameSettingsFeatureProviderGoogleImpl();
-        }
-        return mGameSettingsFeatureProvider;
     }
 }
