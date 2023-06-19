4c044a9a1bbd44c5ffb5a3c8b68de49b2aa81f15
Ido Ben-Hur <idoybh2@gmail.com>
2023-06-19 22:27:28 +0300

    Settings: Allow separating ringer & notification sound streams
    Using the newly added device config (QPR3).
    AOSP have already implemented listeners for this to be runtime,
    but it seems like they never tested settings side properly - especially when the toggle is at the same page.
    After fixing some funny logic errors it now updates just fine.
    base side needs no extra modifications, the observers there are programmed correctly.
    
    Change-Id: Ic171b8411409e8936db7d22ed45e21723e6f66d6

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -39,6 +39,7 @@ import com.android.settings.core.OnActivityResultListener;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.sound.HandsFreeProfileOutputPreferenceController;
+import com.android.settings.sound.SeparateNotificationPreferenceController;
 import com.android.settings.widget.PreferenceCategoryController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.instrumentation.Instrumentable;
@@ -311,7 +312,10 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
         final ScreenshotSoundPreferenceController screenshotSoundPreferenceController =
                 new ScreenshotSoundPreferenceController(context, fragment, lifecycle);
+        final SeparateNotificationPreferenceController separateNotificationPreferenceController =
+                new SeparateNotificationPreferenceController(context);
 
+        controllers.add(separateNotificationPreferenceController);
         controllers.add(dialPadTonePreferenceController);
         controllers.add(inCallNotificationsPreferenceController);
         controllers.add(screenLockSoundPreferenceController);
